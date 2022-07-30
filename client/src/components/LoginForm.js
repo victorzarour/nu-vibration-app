80ad9e25dc96b1377fbe99b5126e31d2ba170213
Ana Zitzow <anasempai89@gmail.com>
2022-07-29 23:27:22 -0700

    login done

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,19 +2,29 @@ import { Button } from "./Button";
 import { FormInput } from "./FormInput";
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+<<<<<<< HEAD
 import "./LoginForm.css"
+=======
+>>>>>>> 526c0b3 (login done)
 
 // TO DO GET ERRORS TO DISPLAY ON PAGE
 
 const LoginForm = ({ setUser }) => {
+<<<<<<< HEAD
 
   const [response, setResponse] = useState(false)
 
+=======
+>>>>>>> 526c0b3 (login done)
   const [ formData, setFormData ] = useState({
     username: "",
     password: ""
   });
+<<<<<<< HEAD
   const [ errors, setErrors ] = useState([]);
+=======
+  const [ , setErrors ] = useState([]);
+>>>>>>> 526c0b3 (login done)
 
   const history = useHistory();
 
@@ -47,52 +57,49 @@ const LoginForm = ({ setUser }) => {
         });
       }
     });
-    setResponse(true)
   };
 
   return (
-    // <form onSubmit={ handleSubmit }>
-    //   <FormInput
-    //   label="Username"
-    //   type="text"
-    //   required
-    //   onChange={ handleChange }
-    //   name="username"
-    //   value={ username }
-    //   />
+    <form onSubmit={ handleSubmit }>
+      <FormInput
+      label="Username"
+      type="text"
+      required
+      onChange={ handleChange }
+      name="username"
+      value={ username }
+      />
 
-    //   <FormInput
-    //   label="Password"
-    //   type="password"
-    //   required
-    //   onChange={ handleChange }
-    //   name="password"
-    //   value={ password }
-    //   />
-    //   <Button type="submit">Login</Button>
-    //   </form>
-
-      <section className='login_body'>
-      <div className='userDiv'>
-        <form className={`${!response ? 'userform' : 'hidden'}`} autoComplete='off' onSubmit={handleSubmit}>
-          <h2>LOGIN</h2>
-            <input
-              type='text' required label='Username' name='username' onChange={handleChange} value={username} placeholder="USERNAME"
-            />
-            <input
-              type='text' required label='Password' name='password' onChange={handleChange} value={password} placeholder="PASSWORD"
-            />
-            <button type="submit" className="user-form-btn">SUBMIT</button>
-        </form>
-      <div className={`${response ? 'userform' : 'hidden'}`}>
-        <h2>WELCOME, {username}</h2>
-      </div>
-      </div>
-     </section>
-     
-
+      <FormInput
+      label="Password"
+      type="password"
+      required
+      onChange={ handleChange }
+      name="password"
+      value={ password }
+      />
+      <Button type="submit">Login</Button>
+    </form>
 
   );
 };
 
 export default LoginForm;
+
+// <section className='login_body'>
+// <div className='userDiv'>
+//   <form className={`${!response ? 'userform' : 'hidden'}`} autoComplete='off' onSubmit={handleSubmit}>
+//     <h2>LOGIN</h2>
+//       <input
+//         type='text' required label='Username' name='username' onChange={handleChange} value={username} placeholder="USERNAME"
+//       />
+//       <input
+//         type='text' required label='Password' name='password' onChange={handleChange} value={password} placeholder="PASSWORD"
+//       />
+//       <button type="submit" className="user-form-btn">SUBMIT</button>
+//   </form>
+// <div className={`${response ? 'userform' : 'hidden'}`}>
+//   <h2>WELCOME, {username}</h2>
+// </div>
+// </div>
+// </section>
