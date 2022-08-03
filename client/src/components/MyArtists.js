4e0a09762209ce717c7ea39ad36db1c4ebfb3b68
Victor <vx_zz@hotmail.com>
2022-08-03 11:39:37 -0400

    testing MyArtists.

--- client/src/components/MyArtists.js
+++ client/src/components/MyArtists.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from 'react';
 import "./AllArtistsPage.css";
+import ArtistThumbnail from './ArtistThumbnail';
 
 const MyArtists = ( { currentUser } ) => {
+
+const [userArtists, setUserArtists] = useState([])
 
     useEffect(() => {
         fetch(`/users/${currentUser.id}`)
         .then(res => res.json())
-        .then(user => console.log(user))
+        .then(user => setUserArtists(user.user_artists))
     }, [])
 
   return (
     <div >
-
+        {userArtists.map(userArtist => <ArtistThumbnail artist={userArtist}/>)}
   </div>
   );
 };
